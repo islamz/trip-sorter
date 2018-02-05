b713d6bf5b395e7f18520c2597f8e43a6ca45507
Islam Zuhair <islamzuhair@gmail.com>
2018-02-05 02:13:50 +0200

    Update Train.php

--- trip-sorter/Transportations/Train.php
+++ trip-sorter/Transportations/Train.php
@@ -2,16 +2,19 @@
 
 namespace Transportations;
 
-class Train extends AbstractTransportations{
-	
+class Plane extends AbstractTransportations{
+
 	public function __construct($boarding){
 		$this->from = $boarding['from'];
 		$this->to 	 = $boarding['to'];
-		$this->trip = $boarding['trip'];
+		$this->flight = $boarding['flight'];
 		$this->seat = $boarding['seat'];
+		$this->gate = $boarding['gate'];
+		$this->notes = $boarding['notes'];
+
 	}
 
 	public function getMessage(){
-		return 'Take train '.$this->trip.' From '.$this->from.' To '.$this->to.'. '.'Sit in seat '.$this->seat."<br />";
+		return 'From '.$this->from.' take flight '.$this->flight.' to '.$this->to.'. Gate '.$this->gate.' seat '.$this->seat.' Baggage drop at ticket counter '.$this->notes."<br />";
 	}
 }
